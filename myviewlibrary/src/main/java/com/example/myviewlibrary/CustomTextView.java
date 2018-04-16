81b87ae6f900e6c29b302030ef3e88eb5d0206ef
theonetech25 <theonetech25@theonetech.local>
2018-04-16 17:08:22 +0530

    second commit with custom fonts ttf file

--- myviewlibrary/src/main/java/com/example/myviewlibrary/CustomTextView.java
+++ myviewlibrary/src/main/java/com/example/myviewlibrary/CustomTextView.java
@@ -14,25 +14,24 @@ public class CustomTextView extends TextView{
 
     public CustomTextView(Context context) {
         super(context);
-        Typeface face=Typeface.createFromAsset(context.getAssets(), "Helvetica_Neue.ttf");
+        Typeface face=Typeface.createFromAsset(context.getAssets(), "fonts/pacifico.ttf");
         this.setTypeface(face);
     }
 
     public CustomTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        Typeface face=Typeface.createFromAsset(context.getAssets(), "Helvetica_Neue.ttf");
+        Typeface face=Typeface.createFromAsset(context.getAssets(), "fonts/pacifico.ttf");
         this.setTypeface(face);
     }
 
     public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        Typeface face= Typeface.createFromAsset(context.getAssets(), "Helvetica_Neue.ttf");
+        Typeface face= Typeface.createFromAsset(context.getAssets(), "fonts/pacifico.ttf");
         this.setTypeface(face);
     }
 
     protected void onDraw (Canvas canvas) {
         super.onDraw(canvas);
-
 
     }
 }
